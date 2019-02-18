fb3b70a3fd56e9c663b89ec41df8616aa2a3cd31
gopal <gopal@gopi>
2019-02-17 18:49:00 -0800

    change in endpont

--- FederalGov/src/main/java/com/gov/ssn/endpoint/SsnValidatorEndpointImpl.java
+++ FederalGov/src/main/java/com/gov/ssn/endpoint/SsnValidatorEndpointImpl.java
@@ -1,5 +1,7 @@
 package com.gov.ssn.endpoint;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -14,6 +16,8 @@ import com.gov.ssn.service.ARSsnService;
 
 @Endpoint
 public class SsnValidatorEndpointImpl{
+	
+	private static final Logger log = LoggerFactory.getLogger(SsnValidatorEndpointImpl.class);
 
 	@Autowired
 	private ARSsnService service;
@@ -30,23 +34,34 @@ public class SsnValidatorEndpointImpl{
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "IndvDetailRequest")
 	@ResponsePayload
 	public IndvDetailResponse ssnValidate(@RequestPayload IndvDetailRequest indvDetailRequest) {
-		System.out.println("in endpoint method");
-		IndvDetailResponse indvDetailRessponse = new IndvDetailResponse();
+		log.info("in endpoint method");
+		IndvDetailResponse indvDetailRessponse = null;
 		IndvDetailType detailType = null;
+		log.info("invoke service class method");
 		//invoke service layer method
 		SsnModel model = service.getUserBySsnAndDob(indvDetailRequest.getSsn(), indvDetailRequest.getDob());
 		//validate ssn no is available or not
 		if(model!=null) {
+			log.info("in if(model!=null) "+model.toString());
+			//create response object
+			indvDetailRessponse = new IndvDetailResponse();
 			detailType = new IndvDetailType();
+			//set values to detailType obj
 			detailType.setFirstName(model.getFirstName());
 			detailType.setLastName(model.getLastName());
 			detailType.setDob(model.getDob());
-			detailType.setSsn(model.getSsn());		
+			detailType.setSsn(model.getSsn());	
+			log.info("out if(model!=null) "+detailType.toString());
+			
+			//set type to response
+			indvDetailRessponse.setIndvDetail(detailType);
+			
+			log.info("out endpoint method "+indvDetailRessponse.getIndvDetail() );
+			//return response to consumer
+			return indvDetailRessponse;
+		}else {
+			return indvDetailRessponse;
 		}
-		//set type to response
-		indvDetailRessponse.setIndvDetail(detailType);
-		//return response to consumer
-		return indvDetailRessponse;
 	}
 
 }
